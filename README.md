dba8bb16774bb899766ea1a326a66e957dba187f
Leon <l2m2lq@gmail.com>
2020-10-28 20:23:00 +0800

    upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # L2DxfUtil
+
+
+
+## Usage
+
+- DXF to Gerber
+
+    ```bash
+    $ dxf2gerber_app.exe demo.dxf
+    ```
+
+## thirdparty
+
+- [dxflib](https://qcad.org/en/90-dxflib)
--- src/dxf2gerber_app/main.cpp
+++ src/dxf2gerber_app/main.cpp
@@ -1,17 +1,29 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
 #include "../dxf2gerber/l2_dxf2gerber.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QString err;
-    bool ok = L2_Dxf2Gerber::run("E:/01.dxf", "E:/", &err);
-    if (!ok) {
-        qDebug() << "Error: " << err;
-        qApp->quit();
+    if (argc >= 2) {
+        QString dxf_path = argv[1];
+        if (!QFile::exists(dxf_path)) {
+            qFatal("The specified path does not exist.");
+            return 1;
+        }
+        QFileInfo finfo(dxf_path);
+        QString err;
+        qInfo() << finfo.absoluteFilePath() << finfo.absoluteDir().absolutePath();
+        bool ok = L2_Dxf2Gerber::run(finfo.absoluteFilePath(), finfo.absoluteDir().absolutePath(), &err);
+        if (!ok) {
+            qFatal("Convert failed. Error: ", err);
+            return 1;
+        }
     }
-    qDebug() << "Fuck Me.";
+
     return 0;
 }
