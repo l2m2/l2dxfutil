198d13cbaa14dc5860e687464502b941dfa3d501
l2m2 <l2m2lq@gmail.com>
2020-11-12 23:23:13 +0800

    upload

--- src/dxf2gerber/l2_dxf2gerber.cpp
+++ src/dxf2gerber/l2_dxf2gerber.cpp
@@ -8,6 +8,64 @@
 #include "../dxflib/dl_dxf.h"
 #include "l2_dxfadapter.h"
 
+static void write(const QString &layer, const QVariantList &data, QTextStream &out)
+{
+    QMap<QString, int> devices;
+    for (auto obj : data) {
+        QVariantMap objMap = obj.toMap();
+        if (objMap.contains("device")) {
+            devices.insert(objMap.value("device").toString(), 0);
+        }
+    }
+
+    out << "%FSLAX36Y36*%\n";
+    out << "%MOMM*%\n";
+    out << "%LN" << layer << "*%\n";
+
+    int counter = 10;
+    for (auto d : devices.keys()) {
+        out << "%ADD" << counter << d << "%\n";
+        devices[d] = counter;
+        counter += 1;
+    }
+
+    QString current_device;
+    for (auto obj : data) {
+        QVariantMap objMap = obj.toMap();
+        if (objMap.contains("command")) {
+            out << objMap.value("command").toString() << "\n";
+        } else {
+            QString device = objMap.value("device").toString();
+            if (device != current_device) {
+                out << "G54D" << devices[device] << "*\n";
+                current_device = device;
+            }
+            QString line("");
+            if (objMap.contains("mode")) {
+                line.append(objMap.value("mode").toString());
+            }
+            if (objMap.contains("x")) {
+                line.append(QString("X%1").arg(qRound(objMap.value("x").toDouble() * 1000000)));
+            }
+            if (objMap.contains("y")) {
+                line.append(QString("Y%1").arg(qRound(objMap.value("y").toDouble() * 1000000)));
+            }
+            if (objMap.contains("i")) {
+                line.append(QString("I%1").arg(qRound(objMap.value("i").toDouble() * 1000000)));
+            }
+            if (objMap.contains("j")) {
+                line.append(QString("J%1").arg(qRound(objMap.value("j").toDouble() * 1000000)));
+            }
+            if (objMap.contains("style")) {
+                line.append(objMap.value("style").toString());
+            }
+            out << line << "*\n";
+        }
+    }
+
+    out << "M02*\n";
+}
+
 bool L2_Dxf2Gerber::toDir(const QString &dxfPath, const QString &gerberDir, QString *err)
 {
     if (!QFile::exists(dxfPath)) {
@@ -40,61 +98,8 @@ bool L2_Dxf2Gerber::toDir(const QString &dxfPath, const QString &gerberDir, QStr
                 return false;
             }
 
-            QMap<QString, int> devices;
-            for (auto obj : it.value()) {
-                QVariantMap objMap = obj.toMap();
-                if (objMap.contains("device")) {
-                    devices.insert(objMap.value("device").toString(), 0);
-                }
-            }
-
             QTextStream out(&file);
-            out << "%FSLAX36Y36*%\n";
-            out << "%MOMM*%\n";
-            out << "%LN" << it.key() << "*%\n";
-
-            int counter = 10;
-            for (auto d : devices.keys()) {
-                out << "%ADD" << counter << d << "%\n";
-                devices[d] = counter;
-                counter += 1;
-            }
-
-            QString current_device;
-            for (auto obj : it.value()) {
-                QVariantMap objMap = obj.toMap();
-                if (objMap.contains("command")) {
-                    out << objMap.value("command").toString() << "\n";
-                } else {
-                    QString device = objMap.value("device").toString();
-                    if (device != current_device) {
-                        out << "G54D" << devices[device] << "*\n";
-                        current_device = device;
-                    }
-                    QString line("");
-                    if (objMap.contains("mode")) {
-                        line.append(objMap.value("mode").toString());
-                    }
-                    if (objMap.contains("x")) {
-                        line.append(QString("X%1").arg(qRound(objMap.value("x").toDouble() * 1000000)));
-                    }
-                    if (objMap.contains("y")) {
-                        line.append(QString("Y%1").arg(qRound(objMap.value("y").toDouble() * 1000000)));
-                    }
-                    if (objMap.contains("i")) {
-                        line.append(QString("I%1").arg(qRound(objMap.value("i").toDouble() * 1000000)));
-                    }
-                    if (objMap.contains("j")) {
-                        line.append(QString("J%1").arg(qRound(objMap.value("j").toDouble() * 1000000)));
-                    }
-                    if (objMap.contains("style")) {
-                        line.append(objMap.value("style").toString());
-                    }
-                    out << line << "*\n";
-                }
-            }
-
-            out << "M02*\n";
+            write(it.key(), it.value(), out);
             file.close();
         }
     }
